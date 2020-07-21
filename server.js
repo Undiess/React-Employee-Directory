bd519e5a63894f7709371371ee17884e5aa2268c
Undiess <nguyen.andy67@gmail.com>
2020-07-21 14:56:18 +0930

    fixed code

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+const publicPath = path.join(__dirname, './public')
+
+app.use(express.static(publicPath));
+
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+    console.log('Server is up!');
+ });
