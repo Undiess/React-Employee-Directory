e748ff5191c180c0b2739b9752ab9385fa715671
Undiess <nguyen.andy67@gmail.com>
2020-07-21 14:30:56 +0930

    added two filters

--- src/App.js
+++ src/App.js
@@ -8,10 +8,27 @@ class App extends Component {
     employees
   };
 
+  filterFrontEnd = () => { 
+    const employees = this.state.employees.filter(employee => employee.occupation === "Front End Developer");
+    this.setState({employees})
+  };
+
+  filterBackEnd = () => { 
+    const employees = this.state.employees.filter(employee => employee.occupation === "Back End Developer");
+    this.setState({employees})
+  };
+
   render(){
-    
     return( 
       <div>
+        <nav className="navbar navbar-light bg-light">
+          <div>
+            <button onClick = {this.filterBackEnd}type="button" className="btn btn-outline-secondary"> Filter Back End</button>
+            <button onClick = {this.filterFrontEnd} type="button" className="btn btn-outline-secondary"> Filter Front End</button>
+          </div>
+          
+        </nav>
+        
         {this.state.employees.map(employees =>( 
           <Card 
             name = {employees.name}
